df402b790d5ad975461b516a3ae358681d57c72d
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2024-02-21 12:44:35 +0100

    Disable iOS/tvOS 17.2 and macOS 14.2 GameKit API from visionOS custom way as #available modifier seems to not being working

--- plug-ins/Apple.GameKit/Native/GameKitWrapper/GKMatchmaker.swift
+++ plug-ins/Apple.GameKit/Native/GameKitWrapper/GKMatchmaker.swift
@@ -117,6 +117,7 @@ public func GKMatchMaker_FindMatchedPlayers
     onError: @escaping NSErrorCallback
 )
 {
+#if !os(visionOS)
     if #available(iOS 17.2, tvOS 17.2, macOS 14.2, *) {
         let gkMatchMaker = Unmanaged<GKMatchmaker>.fromOpaque(gkMatchmakerPtr).takeUnretainedValue()
         let gkMatchRequest = Unmanaged<GKMatchRequest>.fromOpaque(gkMatchRequestPtr).takeUnretainedValue()
@@ -137,6 +138,7 @@ public func GKMatchMaker_FindMatchedPlayers
         // API not available
         onError(taskId, Unmanaged.passRetained(NSError.init(domain: "GKMatchmaker", code: GKErrorCodeExtension.unsupportedOperationForOSVersion.rawValue, userInfo: nil)).toOpaque())
     }
+#endif
 }
 
 @_cdecl("GKMatchmaker_AddPlayers")
@@ -194,6 +196,7 @@ public func GKMatchmaker_QueryQueueActivity
     onError: @escaping NSErrorCallback
 )
 {
+#if !os(visionOS)
     if #available(iOS 17.2, tvOS 17.2, macOS 14.2, *) {
         let gkMatchmaker = Unmanaged<GKMatchmaker>.fromOpaque(gkMatchmakerPtr).takeUnretainedValue();
         gkMatchmaker.queryQueueActivity(queueName.toString(), withCompletionHandler: { numPlayers, error in
@@ -208,6 +211,7 @@ public func GKMatchmaker_QueryQueueActivity
         // API not available
         onError(taskId, Unmanaged.passRetained(NSError.init(domain: "GKMatchmaker", code: GKErrorCodeExtension.unsupportedOperationForOSVersion.rawValue, userInfo: nil)).toOpaque())
     }
+#endif
 }
 
 @_cdecl("GKMatchmaker_QueryPlayerGroupActivity")
